1585e4529148f65d7543fd99414194e6cc117f32
Erik Lumme <erik.lumme@gmail.com>
2020-08-30 19:13:38 +0300

    Improve pathing

--- server/src/main/java/org/vaadin/erik/game/ai/ServerAI.java
+++ server/src/main/java/org/vaadin/erik/game/ai/ServerAI.java
@@ -6,6 +6,7 @@ import org.vaadin.erik.game.ai.pathing.PathingManager;
 import org.vaadin.erik.game.ai.step.RecordedStepFactory;
 import org.vaadin.erik.game.ai.step.Step;
 import org.vaadin.erik.game.server.Server;
+import org.vaadin.erik.game.shared.Constants;
 import org.vaadin.erik.game.shared.Direction;
 import org.vaadin.erik.game.shared.GameMath;
 import org.vaadin.erik.game.shared.Player;
@@ -17,7 +18,9 @@ import static org.vaadin.erik.game.ai.pathing.PathingManager.EMPTY_STACK;
 
 public class ServerAI extends Player {
 
-    private static final double SECONDS_BETWEEN_DECISIONS = 2;
+    private static final double SECONDS_BETWEEN_DECISIONS = 0.5;
+    private static final int CLOSE_PLAYER_THRESHOLD = 96;
+
     private final Server server;
 
     private Stack<Step> steps = EMPTY_STACK;
@@ -31,7 +34,17 @@ public class ServerAI extends Player {
     }
 
     public void takeAction(Collection<Player> players, double delta) {
-        secondsSinceLastDecision += delta;
+        secondsSinceLastDecision += delta / 1000;
+
+        // If a player is close, we go by instinct
+        Player closePlayer = getClosePlayer(players);
+        if (closePlayer != null) {
+            steps = EMPTY_STACK;
+            currentStep = null;
+            server.handleCommand(createPlayerCommand(actOnClosePlayer(closePlayer)));
+            return;
+        }
+
         if (secondsSinceLastDecision > SECONDS_BETWEEN_DECISIONS) {
             secondsSinceLastDecision = 0;
             makeNewDecision(players);
@@ -44,6 +57,29 @@ public class ServerAI extends Player {
                 server.handleCommand(createPlayerCommand(currentStep.getCommand(delta)));
             }
         }
+    }
+
+    private Direction[] actOnClosePlayer(Player closePlayer) {
+        if (getPosition().getY() + Constants.BLOCK_SIZE < closePlayer.getPosition().getY()) {
+            // We are above the player, try to hit the player
+            if (getPosition().getX() < closePlayer.getPosition().getX()) {
+                return new Direction[]{ Direction.RIGHT };
+            } else {
+                return new Direction[]{ Direction.LEFT };
+            }
+        }
+
+        if (closePlayer.getPosition().getY() + Constants.BLOCK_SIZE < getPosition().getY()) {
+            // We are below the player, try to avoid the player. Trying to jump is always a smart plan
+            if (getPosition().getX() < closePlayer.getPosition().getX()) {
+                return new Direction[]{ Direction.LEFT, Direction.UP };
+            } else {
+                return new Direction[]{ Direction.RIGHT, Direction.UP };
+            }
+        }
+
+        // When in doubt, try to jump
+        return new Direction[]{ Direction.UP };
     }
 
     private void makeNewDecision(Collection<Player> players) {
@@ -92,6 +128,22 @@ public class ServerAI extends Player {
             }
         }
         return closestPlayer;
+    }
+
+    /**
+     * Finds a very close player somewhat efficiently
+     */
+    private Player getClosePlayer(Collection<Player> players) {
+        for (Player player: players) {
+            if (player == this) {
+                continue;
+            }
+            if (Math.abs(player.getPosition().getX() - getPosition().getX()) < CLOSE_PLAYER_THRESHOLD &&
+                Math.abs(player.getPosition().getY() - getPosition().getY()) < CLOSE_PLAYER_THRESHOLD) {
+                return player;
+            }
+        }
+        return null;
     }
 
     private PlayerCommand createPlayerCommand(Direction[] directions) {
